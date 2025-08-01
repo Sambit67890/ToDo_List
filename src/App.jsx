4f7fff928b096331a69b6f8e71f7f925e507ce64
Sambit Kumar Padhy <sambeetpadhy2000@gmail.com>
2025-08-01 20:59:05 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
-import CountDown from "./CountDown/CountDown"
-import DigitalClock from "./DigitalClock/DigitalClock"
-import StopWatch from "./StopWatch/StopWatch"
 import React from 'react'
 import ToDo_List from "./ToDo_List/ToDo_List"
-import Counter from "./Counter/Counter"
-import PasswordGenerator from "./PasswordGenerator/PasswordGenerator"
 function App() {
   return (
     <>
